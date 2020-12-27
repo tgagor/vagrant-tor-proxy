b44e6a93128a7efe98d79a94a45c02892fc84610
Tomasz Gągor <tgagor+gh@github.com>
2020-12-27 20:39:26 +0100

    Fix adblock for privoxy

--- privoxy/run.sh
+++ privoxy/run.sh
@@ -3,11 +3,4 @@
 # enable forwarding to tor server
 echo "forward-socks5t / ${TOR_HOST}:${TOR_PORT} ." >> /etc/privoxy/config
 
-cat << EOF >> /etc/privoxy/user.action
-{+block-as-image{Facebook "like" and similar tracking URLs.}}
-www.facebook.com/(extern|plugins)/(login_status|like(box)?|activity|fan)\.php
-platform.twitter.com/widgets/follow_button?
-plusone.google.com
-EOF
-
 exec privoxy $@
